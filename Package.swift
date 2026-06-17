9639b8be7391aae8bdd74e16a4c53dcb5d4195a3
Fernando Fernandes <fernando.fernandes@blacklane.com>
2026-06-17 13:54:16 +0200

    Update AppLogger and GCOverseer requirements

--- Package.swift
+++ Package.swift
@@ -18,11 +18,11 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/thatfactory/applogger",
-            from: "0.1.0"
+            from: "1.0.0"
         ),
         .package(
             url: "https://github.com/thatfactory/gcoverseer",
-            from: "0.1.0"
+            from: "0.1.1"
         )
     ],
     targets: [
